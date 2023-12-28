927278fda1de9cfd118069d4129fb444374f6641
송현서 <s22026@gsm.hs.kr>
2023-12-28 19:55:36 +0900

    :lipstick: :: Add icon

---
+++ design-system/src/main/java/com/tree/design_system/icon/AceIcon.kt
@@ -0,0 +1,51 @@
+package com.tree.design_system.icon
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.tree.design_system.R
+
+@Composable
+fun NewsIcon(isClick: Boolean = false) {
+    if (isClick) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_news_click),
+            contentDescription = "News Click Icon"
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.ic_news),
+            contentDescription = "News Icon"
+        )
+    }
+}
+
+@Composable
+fun HomeIcon(isClick: Boolean = false) {
+    if (isClick) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_home_click),
+            contentDescription = "Home Click Icon"
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.ic_home),
+            contentDescription = "Home Icon"
+        )
+    }
+}
+
+@Composable
+fun EventIcon(isClick: Boolean = false) {
+    if (isClick) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_event_click),
+            contentDescription = "Event Click Icon"
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.ic_event),
+            contentDescription = "Event Icon"
+        )
+    }
+}
