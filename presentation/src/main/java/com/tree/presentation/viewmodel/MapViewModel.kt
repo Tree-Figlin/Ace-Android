a1bef423a9e135565a4acda0f9605f58fb71356d
GSM-Minseok <ms0818000@gmail.com>
2023-12-29 00:05:24 +0900

    :sparkles: :: Add MapViewModel

---
+++ presentation/src/main/java/com/tree/presentation/viewmodel/MapViewModel.kt
@@ -0,0 +1,48 @@
+package com.tree.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.LatLng
+import com.tree.domain.usecase.GetLocationUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MapViewModel @Inject constructor(
+    private val getLocationUseCase: GetLocationUseCase
+) : ViewModel() {
+    private val _viewState: MutableStateFlow<ViewState> = MutableStateFlow(ViewState.Loading)
+    val viewState = _viewState.asStateFlow()
+
+    fun handle(event: PermissionEvent) {
+        when (event) {
+            PermissionEvent.Granted -> {
+                viewModelScope.launch {
+                    getLocationUseCase.invoke().collect {
+                        Log.d("testt",it.toString())
+                        _viewState.value = ViewState.Success(it)
+                    }
+                }
+            }
+
+            PermissionEvent.Revoked -> {
+                _viewState.value = ViewState.RevokedPermissions
+            }
+        }
+    }
+}
+
+sealed interface ViewState {
+    object Loading : ViewState
+    data class Success(val location: LatLng?) : ViewState
+    object RevokedPermissions : ViewState
+}
+
+sealed interface PermissionEvent {
+    object Granted : PermissionEvent
+    object Revoked : PermissionEvent
+}
