5c143cfffe5d55fd0933d4e9bddf95ab539958db
송현서 <s22026@gsm.hs.kr>
2023-12-29 04:13:43 +0900

    :lipstick: :: Add AceSubTitleText

---
+++ design-system/src/main/java/com/tree/design_system/component/text/AceSubTitleText.kt
@@ -0,0 +1,31 @@
+package com.tree.design_system.component.text
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.tree.design_system.theme.AceTheme
+
+@Composable
+fun AceSubTitleText(
+    modifier: Modifier,
+    text: String
+) {
+    AceTheme { colors, typography ->
+        Row(
+            modifier = modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text,
+                style = typography.smallTitle,
+                fontWeight = FontWeight.Normal,
+                color = colors.BLACK
+            )
+        }
+    }
+}
