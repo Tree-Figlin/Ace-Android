c382d6ca1faaeb969d2640a19ebe5c8bdfdb7ddf
송현서 <s22026@gsm.hs.kr>
2023-12-28 21:46:16 +0900

    :lipstick: :: Add AceLogoTitleText

---
+++ design-system/src/main/java/com/tree/design_system/component/text/AceLogoTitleText.kt
@@ -0,0 +1,34 @@
+package com.tree.design_system.component.text
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.tree.design_system.icon.AceLogoIcon
+import com.tree.design_system.theme.AceTheme
+
+@Composable
+fun AceLogoTitleText() {
+    AceTheme { colors, typography ->
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AceLogoIcon()
+            Text(
+                text = "ACE",
+                style = typography.largeTitle,
+                fontWeight = FontWeight.SemiBold,
+                color = colors.PRIMARY
+            )
+        }
+    }
+}
