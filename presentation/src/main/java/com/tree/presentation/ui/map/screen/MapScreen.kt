a61212cdf6d147cffd90601c8a3479c2e7418343
GSM-Minseok <ms0818000@gmail.com>
2023-12-28 22:56:53 +0900

    :sparkles: :: Get location permission

--- presentation/src/main/java/com/tree/presentation/ui/map/screen/MapScreen.kt
+++ presentation/src/main/java/com/tree/presentation/ui/map/screen/MapScreen.kt
@@ -1,8 +1,28 @@
 package com.tree.presentation.ui.map.screen
 
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.shouldShowRationale
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MapScreen() {
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { isGrantedMap: Map<String, Boolean> -> }
 
+    LaunchedEffect("Permission") {
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
 }
