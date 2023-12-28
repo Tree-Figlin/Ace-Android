31bef07183de60dadec448a65f0387f4597121fe
송현서 <s22026@gsm.hs.kr>
2023-12-28 23:30:25 +0900

    :lipstick: :: Modify AceBottomNavigationBar

--- design-system/src/main/java/com/tree/design_system/component/bottombar/AceBottomNavigationBar.kt
+++ design-system/src/main/java/com/tree/design_system/component/bottombar/AceBottomNavigationBar.kt
@@ -1,6 +1,8 @@
 package com.tree.design_system.component.bottombar
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -11,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,68 +40,91 @@ fun AceBottomNavigationBar(
 ) {
     if (isVisible) {
         AceTheme { colors, typography ->
-            Row(
-                modifier
-                    .fillMaxWidth()
+            Column(
+                modifier = modifier
                     .height(64.dp)
-                    .background(colors.WHITE)
-                    .padding(horizontal = 32.dp, vertical = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(
+                        color = colors.BLACK.copy(alpha = 0.1f),
+                        shape = RoundedCornerShape(
+                            topStart = 12.dp,
+                            topEnd = 12.dp,
+                            bottomStart = 0.dp,
+                            bottomEnd = 0.dp
+                        )
+                    ),
+                verticalArrangement = Arrangement.Bottom
             ) {
-                Column(
+                Row(
                     modifier = Modifier
-                        .width(48.dp)
-                        .clickable(
-                            indication = null,
-                            interactionSource = remember { MutableInteractionSource() }
-                        ) { onNewsClick() },
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                        .fillMaxWidth()
+                        .height(63.dp)
+                        .background(
+                            color = colors.WHITE,
+                            shape = RoundedCornerShape(
+                                topStart = 12.dp,
+                                topEnd = 12.dp,
+                                bottomStart = 0.dp,
+                                bottomEnd = 0.dp
+                            )
+                        )
+                        .padding(horizontal = 32.dp, vertical = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    NewsIcon(isClick = currentRoute == "News")
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "검색",
-                        style = typography.caption,
-                        color = if (currentRoute == "News") colors.PRIMARY else colors.GREYSCALE3,
-                        fontWeight = FontWeight.Normal
-                    )
-                }
-                Column(
-                    modifier = Modifier
-                        .width(48.dp)
-                        .clickable(
-                            indication = null,
-                            interactionSource = remember { MutableInteractionSource() }
-                        ) { onHomeClick() },
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    HomeIcon(isClick = currentRoute == "Home")
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "홈",
-                        style = typography.caption,
-                        color = if (currentRoute == "Home") colors.PRIMARY else colors.GREYSCALE3,
-                        fontWeight = FontWeight.Normal
-                    )
-                }
-                Column(
-                    modifier = Modifier
-                        .width(48.dp)
-                        .clickable(
-                            indication = null,
-                            interactionSource = remember { MutableInteractionSource() }
-                        ) { onEventClick() },
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    EventIcon(isClick = currentRoute == "Event")
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "이벤트",
-                        style = typography.caption,
-                        color = if (currentRoute == "Event") colors.PRIMARY else colors.GREYSCALE3,
-                        fontWeight = FontWeight.Normal
-                    )
+                    Column(
+                        modifier = Modifier
+                            .width(48.dp)
+                            .clickable(
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() }
+                            ) { onNewsClick() },
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        NewsIcon(isClick = currentRoute == "News")
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "검색",
+                            style = typography.caption,
+                            color = if (currentRoute == "News") colors.PRIMARY else colors.GREYSCALE3,
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .width(48.dp)
+                            .clickable(
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() }
+                            ) { onHomeClick() },
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        HomeIcon(isClick = currentRoute == "Home")
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "홈",
+                            style = typography.caption,
+                            color = if (currentRoute == "Home") colors.PRIMARY else colors.GREYSCALE3,
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .width(48.dp)
+                            .clickable(
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() }
+                            ) { onEventClick() },
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        EventIcon(isClick = currentRoute == "Event")
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "이벤트",
+                            style = typography.caption,
+                            color = if (currentRoute == "Event") colors.PRIMARY else colors.GREYSCALE3,
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
                 }
             }
         }
