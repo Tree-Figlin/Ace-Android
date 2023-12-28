14a3e66a12102b7f66a9d102a2039167e2491fa7
GSM-Minseok <ms0818000@gmail.com>
2023-12-29 00:06:02 +0900

    :sparkles: :: Add MapPreview

--- presentation/src/main/java/com/tree/presentation/ui/map/component/MapPreview.kt
+++ presentation/src/main/java/com/tree/presentation/ui/map/component/MapPreview.kt
@@ -1,22 +1,36 @@
 package com.tree.presentation.ui.map.component
 
+import android.Manifest
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.maps.android.compose.GoogleMap
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun MapPreview() {
-    GoogleMap(
-        modifier = Modifier.fillMaxSize()
-    ) {
+fun MapPreview(
+    navController: NavController,
+    onLunched: () -> Unit,
+    onBack: () -> Unit
+) {
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    )
+    if(permissionState.allPermissionsGranted) {
+        onLunched()
+        GoogleMap(
+            modifier = Modifier.fillMaxSize()
+        ) {
 
+        }
+    } else {
+        onBack()
     }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun MapPreviewPreview() {
-    MapPreview()
 }
